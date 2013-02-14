9c1578d2633a7b7462cc6a2db9f67e964d17b817
Jeremy Goldman <jeremygoldman@MacBook-Pro-with-Retina-Display-2.local>
2013-02-14 13:36:44 -0600

    added variables, and started BOOL function.

--- goldman_jeremy_assignment2.js
+++ goldman_jeremy_assignment2.js
@@ -1,1 +1,32 @@
-alert("JavaScript works!");
+//Jeremy Goldman
+//SDI_Project 2
+//02/10/13
+//
+//   This is a continuation of the last story about a customer who goes to purchase a mobile phone*
+//   For the sake of my sanity, I am not going to include the original story in this project...
+//   Sorry :P
+
+//Variables
+var custInLine = 8;
+var empToHelp = 3;
+var waitTime = 20;
+var linesRequested = 3;
+var lineMax = 5;
+var currentLines = 2;
+var phoneSelection = ["Galaxy S III", "RAZR HD", "iPhone 5", "Droid Incredible LTE"];
+var custCurrentCarrier = "Verizon Wireless";
+var supportedCarriers = ["Verizon Wireless", "Sprint", "AT&T"];
+
+//BOOL Function
+var hasCustCarrier = function (supportedCarriers, custCurrentCarrier){
+	
+	if(supportedCarriers.indexOf(custCurrentCarrier > -1){
+		console.log("Great! It looks like " + custCurrentCarrier + "is one of our supported carriers!");
+		
+	}else{
+		
+		console.log("I'm Sorry.  It looks like we don't support your carrier in store. :(");
+		
+	}
+	
+};
