f29c8ed914b19816affdd56410ea7d7bea0ad238
Jeremy Goldman <jeremygoldman@MacBook-Pro-with-Retina-Display-2.local>
2013-02-14 13:51:48 -0600

    added Procedure function

--- goldman_jeremy_assignment2.js
+++ goldman_jeremy_assignment2.js
@@ -13,20 +13,34 @@ var waitTime = 20;
 var linesRequested = 3;
 var lineMax = 5;
 var currentLines = 2;
+var monthsSinceUpgrade = 28;
 var phoneSelection = ["Galaxy S III", "RAZR HD", "iPhone 5", "Droid Incredible LTE"];
 var custCurrentCarrier = "Verizon Wireless";
 var supportedCarriers = ["Verizon Wireless", "Sprint", "AT&T"];
+
+//Procedure function - No return value
+var recentlyUpgraded = function(monthsSinceUpgrade){
+	
+	if(monthsSinceUpgrade >= 18){
+		console.log("You havent upgraded to a new phone in a while! You picked a great time to come in!");
+	}else{
+		
+		console.log("It looks like you recently got this phone, but lets see what we can do!");
+	}
+	
+};
 
 //BOOL Function
 var hasCustCarrier = function (supportedCarriers, custCurrentCarrier){
 	
 	if(supportedCarriers.indexOf(custCurrentCarrier > -1){
 		console.log("Great! It looks like " + custCurrentCarrier + "is one of our supported carriers!");
+		return true;
 		
 	}else{
 		
 		console.log("I'm Sorry.  It looks like we don't support your carrier in store. :(");
-		
+		return false;
 	}
 	
 };
